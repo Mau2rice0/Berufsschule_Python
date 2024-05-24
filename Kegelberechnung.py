8f6d623f7abc0e1b1481eabcd848c2b89696463a
Maurice Flöthmann <48244051+Mau2rice0@users.noreply.github.com>
2024-05-24 08:28:06 +0200

    Create Kegelberechnung.py

---
+++ Kegelberechnung.py
@@ -0,0 +1,8 @@
+#Volumen von einem Kegel ausrechnen
+#Version 1.0
+#Author Maurice Flöthmann
+
+
+import math #Importieren der Benötigen Bibliothek
+def kegel_volumen(h ,r): #Definieren der Funktion 
+    G = math.pi *r**2 # Ausrechnen des Volumen eines Kegels
